2895edb5bc49950af36bef43614f2ba8023d5c33
MilkyMoonAtNight <92951919+MilkyMoonAtNight@users.noreply.github.com>
2025-10-11 16:48:38 +0200

    Add project files.

---
+++ CampusLearn/Models/Login.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CampusLearn.Models
+{
+    public class Login
+    {
+        
+        public string sEmail { get; set; } = "";
+        public string sPassword { get; set; } = "";
+        bool bRememberMe { get; set; }= false;
+
+    }
+}
